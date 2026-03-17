136d2a10ccef54890684f775ba0aea4aae08c626
Iretiola <abisigairetiola@gmail.com>
2026-03-17 12:12:13 +0100

    restart project

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")
-    JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY", "jwt-secret-key")
-
-    SQLALCHEMY_DATABASE_URI = "sqlite:///tech_hive.db"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
