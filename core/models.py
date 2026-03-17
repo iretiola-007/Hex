2891f4bc1e93b48084d2f02337057622c787396c
Iretiola <abisigairetiola@gmail.com>
2026-03-17 12:46:18 +0100

    profiles and posts features added

--- core/models.py
+++ core/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    avatar = models.ImageField(upload_to='avatars/', blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    image = models.ImageField(upload_to='posts/', blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
